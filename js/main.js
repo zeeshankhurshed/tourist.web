a09ac0c398ae6d44a337fcc0db1adc04b628b1a0
zeeshankhurshed <109623408+zeeshankhurshed@users.noreply.github.com>
2023-05-24 22:55:08 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+let nav_list = document.querySelector('#nav-list');
+
+let hamburger = document.querySelector('.hamburger');
+
+
+hamburger =addEventListener('click', function() {
+    nav_list.classList.toggle('nav_toggle');
+});
+
+
+// let toggleHamburger = document.querySelector('#toggleHamburger');
+// let menuItems = document.querySelector('#menuItems');
+
+// toggleHamburger.addEventListener('click', function() {
+//   menuItems.classList.toggle('show');
+// });
+
+// let nav_list2 = document.querySelectorAll('#nav-list2');
+// let hamburger2 = document.querySelector('.hamburger-info');
+
+// hamburger2.addEventListener('click', function(){
+//     nav_list2.classList.toggle('nav_toggle');
+// })
+
+
+
+// let leftHamburger = document.querySelector('#leftHamburger');
+// let rightHamburger = document.querySelector('#rightHamburger');
+// let leftMenu = document.querySelector('#leftMenu');
+// let rightMenu = document.querySelector('#rightMenu');
+
+// leftHamburger.addEventListener('click', function() {
+//   leftMenu.classList.toggle('menu-open');
+// });
+
+// rightHamburger.addEventListener('click', function() {
+//   rightMenu.classList.toggle('menu-open');
+// });
